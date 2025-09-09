0489af5ffa05f45a64c1f6b34efa7537b4a0c1ac
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-09-09 19:13:40 +0800

    Initial Commit

---
+++ Snips/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snips
+//
+//  Created by Adon Omeri on 9/9/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snips/SnipsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnipsApp.swift
+//  Snips
+//
+//  Created by Adon Omeri on 9/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnipsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
